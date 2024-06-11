68931b9e3c42e67e679cccba902f7d9feaceda30
cyccro <cycrohuh@gmail.com>
2024-06-11 20:20:51 -0300

    created manifest template

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,48 +1,63 @@
 use axum::{
     body::Bytes,
     extract::{DefaultBodyLimit, Multipart},
-    http::{header, Response, StatusCode},
+    http::{header, StatusCode},
     response::{AppendHeaders, IntoResponse},
     routing::post,
     Router,
 };
-use http_body::Frame;
 use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Write};
 use tower_http::cors::CorsLayer;
-use zip::write::SimpleFileOptions;
+use zip::write::{FileOptions, SimpleFileOptions};
 
 #[derive(Serialize, Deserialize)]
 struct FileData {
     content: String,
     name: String,
 }
-
+async fn create_manifest(zip: &mut zip::ZipWriter<File>, options: FileOptions<'_, ()>) {
+    let manifest_preset = std::fs::read("presets/manifest.txt").unwrap();
+    zip.start_file("manifest.json", options).unwrap();
+    zip.write(&manifest_preset).unwrap();
+}
 async fn handle_files(mut multipart: Multipart) -> Result<impl IntoResponse, (StatusCode, String)> {
     let output_name = "merged_files.zip";
-    let zipfile = File::create(&output_name.to_string()).unwrap();
+    let zipfile = File::create(output_name).unwrap();
     let zip_options =
         SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
     let mut zip = zip::ZipWriter::new(zipfile);
-
     while let Some(field) = multipart.next_field().await.unwrap() {
         let file_name = {
             let temp = field.file_name().unwrap().to_string();
-            if let Some(idx) = temp.find('/') {
+            let split = temp.split("/").collect::<Vec<&str>>();
+            if split[1] == "scripts" {
+                format!(
+                    "{}_{}{}",
+                    split[0],
+                    split[1],
+                    &temp[(1 + split[0].len() + split[1].len())..]
+                )
+            } else if let Some(idx) = temp.find('/') {
                 (&temp[idx..]).to_string()
             } else {
                 temp
             }
         };
         let bytes = field.bytes().await.unwrap();
-        if let Ok(_) = zip.start_file(file_name.clone(), zip_options) {
+        if let Ok(_) = zip.start_file(file_name, zip_options) {
+            //Creates a file, if not
+            //error(duplicated file) writes into it
             if let Err(e) = zip.write(&bytes[..]) {
                 println!("Hello {}", e);
                 return Err((StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e)));
             }
+            continue;
         }
     }
+    create_manifest(&mut zip, zip_options).await;
     match zip.finish() {
+        //finishes the zip management
         Ok(_) => {
             let buffer = std::fs::read(output_name).unwrap();
             let bytes = Bytes::from(buffer);
@@ -53,9 +68,13 @@ async fn handle_files(mut multipart: Multipart) -> Result<impl IntoResponse, (St
                     "inline; filename=\"merged_addons.zip\"",
                 ),
             ]);
+            let _ = std::fs::remove_file(output_name);
             Ok((headers, bytes))
         }
-        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e))),
+        Err(e) => {
+            let _ = std::fs::remove_file(output_name);
+            Err((StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e)))
+        }
     }
 }
 pub fn routes() -> Router {

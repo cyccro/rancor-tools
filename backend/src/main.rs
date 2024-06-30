02abca566e4e219703df42498859e403ca648cf2
cyccro <cycrohuh@gmail.com>
2024-06-30 16:12:50 -0300

    Added manifest item_texture and terrain_texture items support

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,6 @@ mod addons;
 mod routes;
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8081").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, routes::routes()).await.unwrap();
 }
